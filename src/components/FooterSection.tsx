a49678c7eef98c0ae10b7061cb9983dc55dfa205
HuynhPPP <tankhuong02@email.com>
2026-03-20 09:56:49 +0700

    update footer

--- src/components/FooterSection.tsx
+++ src/components/FooterSection.tsx
@@ -6,40 +6,47 @@ const FooterSection = () => {
   return (
     <footer className='footer-section'>
       <div className='footer-container'>
-        <p className='footer-tagline'>Đại lý phân phối chính hãng</p>
-        <img
-          src='/Logo_Radio_Shopping.png'
-          alt='Radio Shopping'
-          className='footer-radio-logo'
-        />
-        <h3 className='footer-brand'>Radio Shopping</h3>
-        <h4 className='footer-contact-header'>THÔNG TIN LIÊN HỆ</h4>
-
-        <div className='footer-contact-info'>
-          <div className='footer-address-block'>
-            <p className='footer-contact-label'>Địa chỉ công ty:</p>
-            <p className='footer-contact-text'>
-              <MapPin className='footer-icon-small inline-icon' />{' '}
-              {agentInfo.address}
-            </p>
+        <div className='footer-grid'>
+          <div className='footer-col footer-col-left'>
+            <p className='footer-tagline'>Đại lý phân phối chính hãng</p>
+            <div className='footer-brand-wrapper'>
+              <img
+                src='/Logo_Radio_Shopping.png'
+                alt='Radio Shopping'
+                className='footer-radio-logo'
+              />
+              {/* <h3 className='footer-brand'>Radio Shopping</h3> */}
+            </div>
           </div>
 
-          <div className='footer-address-block'>
-            <p className='footer-contact-label'>Kho hàng Cần Thơ:</p>
-            <p className='footer-contact-text'>
-              <MapPin className='footer-icon-small inline-icon' />{' '}
-              {agentInfo.warehouse}
-            </p>
+          <div className='footer-col footer-col-middle'>
+            <div className='footer-address-block'>
+              <p className='footer-contact-label'>Địa chỉ công ty:</p>
+              <p className='footer-contact-text'>
+                <MapPin className='footer-icon-small inline-icon' />{' '}
+                {agentInfo.address}
+              </p>
+            </div>
+
+            <div className='footer-address-block'>
+              <p className='footer-contact-label'>Kho hàng Cần Thơ:</p>
+              <p className='footer-contact-text'>
+                <MapPin className='footer-icon-small inline-icon' />{' '}
+                {agentInfo.warehouse}
+              </p>
+            </div>
           </div>
 
-          <div className='footer-links-row'>
-            <a href={`tel:${agentInfo.phone}`} className='footer-link'>
-              <Phone className='footer-icon-small' /> {agentInfo.phone}
-            </a>
-            <span className='footer-divider'>|</span>
-            <a href={`mailto:${agentInfo.email}`} className='footer-link'>
-              <Mail className='footer-icon-small' /> {agentInfo.email}
-            </a>
+          <div className='footer-col footer-col-right'>
+            <h4 className='footer-contact-header'>THÔNG TIN LIÊN HỆ</h4>
+            <div className='footer-links-row'>
+              <a href={`tel:${agentInfo.phone}`} className='footer-link'>
+                <Phone className='footer-icon-small' /> {agentInfo.phone}
+              </a>
+              <a href={`mailto:${agentInfo.email}`} className='footer-link'>
+                <Mail className='footer-icon-small' /> {agentInfo.email}
+              </a>
+            </div>
           </div>
         </div>
 
